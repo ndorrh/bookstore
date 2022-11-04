fb0198b96f2ec75db860a28cb2bfc024110dbe4a
ndorrh <ndorrh@gmail.com>
2022-11-04 16:00:41 +0100

    Modified NavBar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
-const NavBar = () => (
-  <nav className="navbar">
-    <h1>Math Magicians</h1>
-    <ul>
-      <li className="home"><Link to="/">BOOKS</Link></li>
-      <li className="quote"><Link to="/CategoryPage">CATEGORY</Link></li>
-    </ul>
-  </nav>
-);
+function Nav() {
+  return (
+    <div>
+      <header className="header">
+        <nav className="nav-bar">
+          <ul className="nav-links">
+            <li className="logo">Bookstore CMS</li>
+            <Link to="/">
+              <li className="book-cat">BOOKS</li>
+            </Link>
+            <Link to="/categories">
+              <li className="book-cat">CATEGORIES</li>
+            </Link>
+          </ul>
+          <div className="avatar">
+            <span>
+              <FontAwesomeIcon className="user-circle" icon={faUserCircle} />
+            </span>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
 
-export default NavBar;
+export default Nav;
