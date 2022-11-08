0bc859b265d642fd3372e0e60792dbe5023049b9
ndorrh <ndorrh@gmail.com>
2022-11-08 14:35:54 +0100

    Combine books reducers and category reducer into rootRreducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { booksSlice } from './books/books';
+import booksSlice from './books/books';
+import categorySlice from './categories/category';
 
 const rootReducer = configureStore({
   reducer: {
     books: booksSlice.reducer,
+    category: categorySlice.reducer,
   },
 });
 
