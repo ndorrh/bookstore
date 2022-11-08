e96ae8f22f1ca885da25ab30a1a05637b5a063c3
ndorrh <ndorrh@gmail.com>
2022-11-08 14:32:07 +0100

    Modify InputBook component to dispatch change when the addBook  button is click

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { booksActions } from '../redux/books/books';
 
 function InputBook() {
   const dispatch = useDispatch();
-  const book = useSelector((state) => state.books.booksList);
-  console.log(book);
   const handleAddBook = () => {
-    dispatch(booksActions.addBook());
+    dispatch(booksActions.addBook({
+      id: 1,
+      title: 'Awa and Other plays',
+      author: 'Ndorrh Oswald',
+
+    }));
   };
   return (
     <div>
