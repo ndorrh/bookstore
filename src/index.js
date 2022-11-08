3cce4bd201b0ad405873feeeaa38be96109b6bc6
ndorrh <ndorrh@gmail.com>
2022-11-08 14:33:29 +0100

    Add rootReducer to index.js to update state globally

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
     <BrowserRouter>
       <Provider store={rootReducer}>
         <App />
       </Provider>
     </BrowserRouter>
+
   </React.StrictMode>,
 );
 
