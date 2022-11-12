3231118b23e9e05f0b703b456e799aae1055708b
ndorrh <ndorrh@gmail.com>
2022-11-12 06:00:57 +0100

    Modify store name from rootReducer to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksSlice from './books/books';
 import categorySlice from './categories/category';
 
-const rootReducer = configureStore({
+const store = configureStore({
   reducer: {
     books: booksSlice.reducer,
     category: categorySlice.reducer,
   },
 });
 
-export default rootReducer;
+export default store;
